bc0d29f84194adae9cf92fcd907784cdc40b94e4
gchen151 <gordonchen@umass.edu>
2022-11-20 14:22:33 -0500

    update

--- app.js
+++ app.js
@@ -4,12 +4,37 @@ const app = express()
 
 // use the express-static middleware
 app.use(express.static("src"))
+app.use(express.urlencoded({
+  extended: false
+}))
+
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 
 // define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+app.get("/hello", function (req, res) {
+  console.log("Heree")
+  res.send("check ")
 })
+
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+	() => console.log(`Example app listening at http://localhost:${3000}`));
+
+app.post('/requestTutor', (req, res) => { 
+  console.log("Hello from requestTutor")
+  res.send("Testing From requestTutor")
+
+});
+app.post('/src/reqTutor.html/requestTutor', (req, res) => {
+  console.log("woeking")
+  res.end()
+})
--- src/reqTutor.js
+++ src/reqTutor.js
@@ -1,9 +1,75 @@
 
+// import request from 'request';
+function updateClient(postData){
+    var clientServerOptions = {
+        uri: '/reqTutor-form',
+        body: JSON.stringify(postData),
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    request(clientServerOptions, function (error, response) {
+        console.log(error,response.body);
+        return;
+    });
+}
+
 document.getElementById("tutee-submit").addEventListener("click", ()=> {
     const yearSelect = document.getElementById("year");
     const name = document.getElementById("tutee-name");
     const email = document.getElementById("tutee-email");
     const courses = document.getElementById("courses");
     const grade = yearSelect.options[yearSelect.selectedIndex].text;
-    alert(grade);
+    const description = document.getElementById("description")
+ 
+    const data = {
+        "Name": name.value,
+        "Email": email.value,
+        "Course": courses.value,
+        "Grade": grade,
+        "Description" : description.value
+    };
+    // console.log(data)
+    // updateClient(data)
+    fetch('/src/reqTutor.html/requestTutor', {
+        method: 'POST',
+        body: JSON.stringify({ "id": 78912 })
+    })
+    .then(response => response)
+    .then(response => console.log(response))
+
+    // fetch('./src/reqTutor.html/requestTutor', {
+    //     method: 'POST',
+    //     body: JSON.stringify(data),
+    //     })  
+
+    //     .then((data) => {
+    //         console.log('Success:????????', data);
+    //     })
+    //     .catch((error) => {
+    //         console.error('Error:', error);
+    //     });
+
+    // var xhr = new XMLHttpRequest();
+    // xhr.open("POST", "/requestTutor", true);
+    // xhr.send(data);
+    // xhr.onload = function() {
+    //     console.log(data);
+    // };
+    // // fetch("/requestTutor", {
+    // //     method:"POST",
+    // //     body: JSON.stringify(data)
+    // //     }).then(result => {
+    // //         // do something with the result
+    // //         console.log("Completed with result:", result);
+    // //     }).catch(err => {
+    // //         // if any error occured, then catch it here
+    // //         console.error(err);
+    // //     });
+
 })
+
+
+
+
