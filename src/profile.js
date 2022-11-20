63ef53d1a498500da66065d37beb2baeb16a3390
gchen151 <gordonchen@umass.edu>
2022-11-20 18:43:22 -0500

    moved profilejs

---
+++ src/profile.js
@@ -0,0 +1,27 @@
+document.getElementById("updateProfileBTN-submit").addEventListener("click", ()=> {
+    const yearSelect = document.getElementById("year");
+    const data = {
+        "Name": document.getElementById("card-name").value,
+        "Email": document.getElementById("email").value,
+        "Course": document.getElementById("course_title").value,
+        "Grade": yearSelect.options[yearSelect.selectedIndex].text,
+        "Description" : document.getElementById("description_box").value,
+    };
+    console.log(data)
+    fetch('/updateTutee', {
+        method: 'POST', // or 'PUT'
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then((response) => response)
+        .then((data) => {
+            console.log('Success:', data);
+            console.log(data)
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+});
