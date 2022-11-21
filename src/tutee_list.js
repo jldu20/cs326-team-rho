7805068f65aa5fac67698b876225e5844ab5d229
Jerry Du <jduomaha@gmail.com>
2022-11-20 23:01:52 -0500

    fixed names

--- src/tutee_list.js
+++ src/tutee_list.js
@@ -1,20 +1,21 @@
-// let table = document.getElementById("myTable");
+let table = document.getElementById("myTable");
 const {MongoClient} = require('mongodb')
 const uri = "mongodb+srv://rhoMember:2bSh5JdVsCuW3TSK@rhobase.faewymn.mongodb.net/test";
 const client = new MongoClient(uri);
 async function run() {
   try {
     const database = client.db("open_source_learning");
-    const movies = database.collection("tutees");
-    await movies.find().forEach(x=> {
-        // alert(x.Name);
-        console.log(x.Name);
+    const tutees = database.collection("tutees");
+    await tutees.find().forEach(x=> {
+
+        // let xd = table.insertRow(-1);
+        // xd.innerHTML = "<td>test</td><td>test</td><td>test</td><td>test</td><td>test</td>";
     })
   } finally {
     await client.close();
   }
 }
-// alert("hello")
+
 run().catch(console.dir);
 // let xd = table.insertRow(-1);
 
