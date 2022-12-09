0951e24bd0f2f100503e1badb59bfcde06d6423a
Jerry Du <jduomaha@gmail.com>
2022-12-09 05:41:48 -0500

    fixed tutee list

--- src/tutee_list.js
+++ src/tutee_list.js
@@ -1,39 +1,15 @@
 
-document.getElementById("dbSearch").addEventListener("click", ()=> {
-
-  fetch('/getTutee',{
-    method:"GET",
-    headers: { 
-      'Accept': 'application/json',
-      'Content-Type': 'application/json' 
-    }
-  })
-  .then(response => response.json());
-
+let table = document.getElementById("myTable");
+const res = fetch('/getTutee',{
+  method:"GET",
+  headers: { 
+    'Accept': 'application/json',
+    'Content-Type': 'application/json' 
+  }
+}).then(response => response.json()).then(arr=>{
+  for(let i = 0; i < arr.length; i++) {
+    let person = arr[i]
+    let newRow = table.insertRow(-1)
+    newRow.innerHTML = `<td>${person.Name}</td><td>${person.Email}</td><td>${person.Grade}</td><td>${person.Description}</td>`;
+  }
 })
-
-
-// const {MongoClient} = require('mongodb')
-// const uri = "mongodb+srv://rhoMember:2bSh5JdVsCuW3TSK@rhobase.faewymn.mongodb.net/test";
-// const client = new MongoClient(uri);
-// async function run() {
-//   try {
-//     const database = client.db("open_source_learning");
-//     const tutees = database.collection("tutees");
-//     await tutees.find().forEach(x=> {
-//         arr.push(x);
-//         let xd = table.insertRow(-1);
-//         xd.innerHTML = `<td>${x.Name}</td><td>${x.Email}</td><td>${x.Grade}</td><td>${x.Description}</td>`;
-//     })
-//     console.log(arr);
-//   } finally {
-//     await client.close();
-//   }
-// }
-
-// run().catch(console.dir);
-// let xd = table.insertRow(-1);
-
-// xd.innerHTML = "<td>test</td><td>test</td><td>test</td><td>test</td><td>test</td>";
-
-
